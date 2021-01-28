b70908f8d1f93c589993c82f2a8ee85dce506e92
webDaulet <web.daulet@gmail.com>
2021-01-29 00:08:44 +0600

    homework-1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import './App.css';
-import Header from './components/Header/Header'
-import Layout from './components/Layout/Layout'
-import Footer from './components/Footer/Footer'
-import bg1 from './images/bg1.jpg';
-import bg2 from './images/bg2.jpg';
+import Header from "./components/Header/index";
 
-const color = 'yellow';
-
-function App() {
+const App = () => {
   return (
     <>
-      <Header
-        title="This is title"
-        descr="This is description"
+      <Header 
+        title="hello"
+        descr="descr"
       />
-      <Layout
-        id='001'
-        urlBg={bg1}
-        title="This is title"
-        descr="This is description"
-      />
-      <Layout
-        id='002'
-        title="This is title"
-        descr="This is description"
-        colorBg={{ color }} />
-      
-      <Layout
-        id='003'
-        urlBg={bg2}
-        title="This is title"
-        descr="This is description"/>
-      
-      <Footer/>
-     
     </>
-  );
+  )
 }
 
 export default App;
